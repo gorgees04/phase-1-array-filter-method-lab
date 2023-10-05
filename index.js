8e0fdf79bf381f1651c71d7d7d7c65166d6689c7
Gorgees Odisho <gorgees041@gmail.com>
2023-10-05 05:12:23 +0300

    DONE WITH FILTER ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, driverName) {
+    return drivers.filter(driver => driver.toLowerCase() === driverName.toLowerCase())
+}
+
+function fuzzyMatch(drivers, driverName) {
+    return drivers.filter(driver => driver.startsWith(driverName))
+}
+
+function matchName(drivers, driverName) {
+    return drivers.filter(driver => driver.name.toLowerCase() === driverName.toLowerCase())
+}
+
+
+
